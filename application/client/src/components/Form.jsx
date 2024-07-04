8a6036460f817cf65061c82f7d840e5d6eea9606
Darren Tan <>
2024-07-04 16:43:20 +0800

    Add simple user form

---
+++ application/client/src/components/Form.jsx
@@ -0,0 +1,170 @@
+/* Interest area: category, text box
+Price preference: affordable/pricey, dropdown
+Trendiness: trendy/unique, dropdown
+Design style: description, text box
+recipient: describe recipient, text box
+recipient location: textbox
+receiving date/time: textbox?
+*/
+import React, { useState } from "react";
+import "../styles/Form.css";
+
+function Form() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [formData, setFormData] = useState({
+        Category: "",
+        Price_Preference: "",
+        Trendiness: "",
+        Design_Style: "",
+        Recipient_Description: "",
+        Recipient_Location: "",
+        Receiving_Date: "",
+    });
+    const [result, setResult] = useState("");
+    const [showResults, setShowResults] = useState(false);
+
+    const handleChange = (event) => {
+        const value = event.target.value;
+        const name = event.target.name;
+        let inputData = { ...formData };
+        inputData[name] = value;
+        setFormData(inputData);
+    };
+
+    // not tested, taken from gfg
+    const handlePredictClick = () => {
+        const url = "http://localhost:5000/predict";
+        setIsLoading(true);
+        console.log("Predict was called with:", formData);
+        const jsonData = JSON.stringify(formData);
+
+        fetch(url, {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+            body:jsonData,
+        })
+        .then((response) => response.json())
+        .then((response) => {
+            setResult(response.Prediction);
+            setIsLoading(false);
+            setShowResults(true);
+        });
+    };
+
+    return (
+        <div className="form-container">
+            <form method="post" acceptCharset="utf-8">
+                <div className="form-group">
+                    <label>Category of Product:
+                        <input
+                        type="text"
+                        className="form-control"
+                        id="Category"
+                        name="Category"
+                        value={formData.Category}
+                        onChange={handleChange}
+                        placeholder="Electronics, Apparel etc."/>
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label>Trendiness of Product:
+                        <select
+                        className="dropdown form-control"
+                        id="Trendiness"
+                        name="Trendiness"
+                        value={formData.Trendiness}
+                        onChange={handleChange}
+                        required>
+                            <option value={""} disabled>
+                                Select Trendy or Unique
+                            </option>
+                            <option value="0">Unique</option>
+                            <option value="1">Trendy</option>
+                        </select>
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label> Product Description:
+                        <input
+                        type="text"
+                        className="form-control"
+                        id="Design_Style"
+                        name="Design_Style"
+                        value={formData.Design_Style}
+                        onChange={handleChange}
+                        placeholder="Describe your ideal product"/>
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label>Price Preference:
+                        <select
+                        className="dropdown form-control"
+                        id="Price_Preference"
+                        name="Price_Preference"
+                        value={formData.Price_Preference}
+                        onChange={handleChange}
+                        required>
+                            <option value={""} disabled>
+                                Select your preferred price
+                            </option>
+                            <option value="0">Affordable</option>
+                            <option value="1">Pricey</option>
+                        </select>
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label> Recipient Description:
+                        <input
+                        type="text"
+                        className="form-control"
+                        id="Recipient_Description"
+                        name="Recipient_Description"
+                        value={formData.Recipient_Description}
+                        onChange={handleChange}
+                        placeholder="Describe your product recipient"/>
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label> Recipient Location:
+                        <input
+                        type="text"
+                        className="form-control"
+                        id="Recipient_Location"
+                        name="Recipient_Location"
+                        value={formData.Recipient_Location}
+                        onChange={handleChange}
+                        placeholder="Location"/>
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label> Receive Product by:
+                        <input
+                        type="text"
+                        className="form-control"
+                        id="Receiving_Date"
+                        name="Receiving_Date"
+                        value={formData.Receiving_Date}
+                        onChange={handleChange}
+                        placeholder="Date and Time"/>
+                    </label>
+                </div>
+                <div className="form-group">
+                    <button
+                    className="predict-btn"
+                    disabled={isLoading}
+                    onClick={!isLoading ? handlePredictClick : null}
+                    >
+                    Get Product Recommendations
+                    </button>
+                </div>
+            </form>
+            <div className="results-container">
+            </div>
+        </div>
+    )
+};
+
+export default Form;
--- application/flaskapp/app.py
+++ application/flaskapp/app.py
@@ -1,17 +1,36 @@
-from flask import Flask, request
+# Commented parts not tested, just taken from gfg, supposedly code for integrating model into the app.
+
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+#import pandas as pd
+#from flask_cors import CORS
+#import pickle
 
 load_dotenv()
 app = Flask(__name__)
 
+#CORS(app, resources={r"/*":{"origins:"*""}})
+#model = pickle.load(open('ml_model.pkl', 'rb'))
+
 MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def yo():
-	return "<p>yo mama</p>"
+    data = {"message": "yo mama"}
+    return jsonify(data)
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     try:
+#         data = request.get_json()
+#         query_df = pd.Dataframe([data])
+#         prediction = model.predict(query_df)
+#         return jsonify({'Prediction': list(prediction)})
+#     except Exception as e:
+#         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
